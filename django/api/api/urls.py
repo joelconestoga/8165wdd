4769b7bfa83c83916c47e51384c5a620e7af1cad
joel matsu <matsu.joel@gmail.com>
2017-04-23 18:13:41 -0400

    accepting url=localhost:8000 without requiring /frontend

--- django/api/api/urls.py
+++ django/api/api/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^frontend/', include('frontend.urls')),
     url(r'^backend/', include('backend.urls')),
+    url(r'^$', views.frontend, name='frontend'),
 ]
